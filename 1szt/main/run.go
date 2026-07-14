3326a9e8c4c3ee07a902fc4b244c956caf311f78
鲸玄微 <me@jingxuanwei.com>
2026-07-14 16:51:47 +0800

    添加 env 配置管理工具及启动文件

---
+++ 1szt/main/run.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"1szt/motd"
+)
+
+func main() {
+	// 欢迎语
+	motd.Run()
+
+
+	select {}
+}
---
+++ 1szt/motd/run.go
@@ -0,0 +1,10 @@
+package motd
+
+import "fmt"
+
+func Run() {
+	fmt.Println("========================================")
+	fmt.Println("          SBCC 控制中心启动！")
+	fmt.Println("========================================")
+	fmt.Println("正在启动各个模块...")
+}
